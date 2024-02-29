aeb5633e5e6ea36f74ec0f1174ca7a36fb381325
xupremix <xupremix.me@gmail.com>
2024-02-29 18:48:43 +0100

    save

--- src/app/mainview/editor/playground.rs
+++ src/app/mainview/editor/playground.rs
@@ -1,4 +1,6 @@
+use std::cell::RefCell;
 use std::path::PathBuf;
+use std::rc::Rc;
 use fltk::enums::Event;
 use fltk::enums::FrameType;
 use fltk::frame::Frame;
@@ -11,12 +13,12 @@ use tch::Device;
 use crate::components::activation_functions::ActivationFunctionType;
 use crate::components::layers::LayerType;
 use crate::components::NNComponent;
-use crate::utils::consts::BG_COLOR;
+use crate::utils::consts::{BASE_COMPONENT_HEIGHT, BASE_COMPONENT_WIDTH, BG_COLOR};
 use crate::utils::CustomDialog;
 use crate::utils::loss_fn::LossFunction;
 
 pub(crate) struct Playground {
-    draw_area: Window,
+    draw_area: Rc<RefCell<Window>>,
     components: Vec<NNComponent>,
     first: Option<usize>,
 }
@@ -28,44 +30,51 @@ impl Playground {
         draw_area.end();
 
         Self {
-            draw_area,
+            draw_area: Rc::new(RefCell::new(draw_area)),
             components: vec![],
             first: None,
         }
     }
 
     pub(crate) fn add_layer(&mut self, layer: LayerType) {
+        let draw_area = self.draw_area.clone();
+        let label = *layer;
         let mut component = Frame::default()
-            .with_size(100, 60)
-            .center_of(&self.draw_area);
+            .with_size(BASE_COMPONENT_WIDTH.max(label.len() as i32 * 11), BASE_COMPONENT_HEIGHT)
+            .center_of(&*draw_area.borrow());
         component.set_frame(FrameType::FlatBox);
         component.set_color(Color::White);
         let mut inner_component = Frame::default()
-            .with_size(98, 58)
+            .with_size(component.w() - 4, component.h() - 4)
             .center_of(&component)
-            .with_label(*(layer.clone()));
+            .with_label(*layer);
         inner_component.set_label_color(Color::White);
         inner_component.set_frame(FrameType::FlatBox);
         inner_component.set_color(BG_COLOR);
-        self.draw_area.add(&component);
-        self.draw_area.add(&inner_component);
-        self.draw_area.redraw();
+        draw_area.borrow_mut().add(&component);
+        draw_area.borrow_mut().add(&inner_component);
+        draw_area.borrow_mut().redraw();
         let mut set = false;
-        inner_component.handle(move |component, event| {
+        let mut prev = (-1, -1);
+        inner_component.handle(move |inner_component, event| {
             match event {
-                Event::Push if !set => {
-                    set = true;
+                Event::Push => {
+                    if !set {
+                        set = true;
+                    }
+                    prev = fltk::app::event_coords();
                     true
                 },
                 Event::Drag if set => {
-                    let (x, y) = (fltk::app::event_x(),fltk::app::event_y()) ;
-                    component.resize(
-                        component.x() - x,
-                        component.y() - y,
-                        component.w(),
-                        component.h(),
+                    let new_coords = fltk::app::event_coords();
+                    let (new_x, new_y) = (
+                        component.x() + (new_coords.0 - prev.0),
+                        component.y() + (new_coords.1 - prev.1),
                     );
-                    eprintln!("X: {}, Y: {}", x, y);
+                    prev = new_coords;
+                    component.set_pos(new_x, new_y);
+                    inner_component.set_pos(new_x + 2, new_y + 2);
+                    draw_area.borrow_mut().redraw();
                     true
                 },
                 _ => false,
